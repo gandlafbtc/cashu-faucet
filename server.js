03d7e5c86afa39a4cdfe27d8c73f0f118d8f1579
gandlaf21 <gandlaf@proton.me>
2023-02-14 08:57:34 +0100

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const { CashuMint, CashuWallet, getEncodedProofs } = require("@gandlaf21/cashu-ts")
 const { Faucet } = require ("@gandlaf21/cashu-tools")
 const rateLimit = require('express-rate-limit')
+const cors = require('cors');
 
 const express = require('express')
 const app = express()
@@ -11,13 +12,14 @@ const port = process.env.PORT
 let faucet
 
 const limiter = rateLimit({
-	windowMs: 15 * 1000, // 15 seconds
+  windowMs: 15 * 1000, // 15 seconds
 	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
 app.use(limiter)
+app.use(cors());
 
 app.get('/', (req, res) => {
     let responseString ="Token has already been claimed! waiting for next token..."
@@ -31,6 +33,7 @@ app.get('/', (req, res) => {
 app.get('/charge', async (req, res) => {
     const token = req.query.token
     const message  = await faucet.charge(token)
+    console.log("all Tokens:", getEncodedProofs(balance ,[{url:process.env.MINT_URL, keysets: [...new Set(faucet.currentToken.map(t=>t.id))]}]))
     res.send({message})
   })
 
@@ -40,7 +43,6 @@ app.listen(port, async () => {
     const cashuWallet = new CashuWallet(keys, cashuMint)
     faucet = new Faucet(cashuWallet, process.env.FAUCET_INTERVAL, process.env.SATS_PER_INTERVAL)
     await faucet.start()
-
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
