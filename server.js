ee54eaa7ea6a35a74d7f2e20337c0d7a6b11e5d5
gandlaf21 <gandlaf@proton.me>
2023-02-18 12:41:16 +0100

    param for request timeout

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const port = process.env.PORT
 let faucet
 
 const limiter = rateLimit({
-  windowMs: 15 * 1000, // 15 seconds
-	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: process.env.REQUEST_TIMEOUT_INTERVAL, // 15 seconds
+	max: process.env.REQUEST_TIMEOUT_MAX, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
